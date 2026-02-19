36a833bed7fb56bc095910f2614ba00004c8c67a
sofiia <sofia.velikaua@gmail.com>
2026-02-19 11:01:44 +0200

    new pull

---
+++ src/task-6.ts
@@ -0,0 +1,8 @@
+function getFirstElement <T>(arr: T[]): T {
+    return arr[0];
+  }
+  
+  getFirstElement([1, 2, 3]);           // 1
+  getFirstElement(["a", "b", "c"]);     // "a"
+  getFirstElement([true, false, true]); // true
+  
